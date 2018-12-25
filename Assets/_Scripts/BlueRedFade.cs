3b99a5c78a57e38dbff8106e7c35686b809ba6b1
tvoysosed <32384768+tvoysosed@users.noreply.github.com>
2018-12-25 03:15:52 +0300

    Add files via upload

---
+++ Assets/_Scripts/BlueRedFade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlueRedFade : MonoBehaviour {
+
+    private Image background;
+    private float speed = 1.0f;
+
+
+    // Используем для инициализации
+    void Start () {
+        background = gameObject.GetComponent<Image>();
+    }
+	
+	// Обновляем картинку один раз за кадр  
+	void Update () {
+
+        float t = (Mathf.Sin(Time.time * speed) + 1) / 2.0f;
+
+        background.color = Color.Lerp(Color.red, Color.blue, t);
+
+    }
+}
---
+++ Assets/_Scripts/PlayerPrefBackground.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefBackground : MonoBehaviour {
+
+    public Camera mainCamera;
+
+	// Используем это для инициализации     
+	void Start () {
+        if (PlayerPrefs.HasKey("Vote"))
+        {
+            switch (PlayerPrefs.GetString("Vote"))
+            {
+                case "Trump":
+                    mainCamera.backgroundColor = Color.red;
+                    break;
+                case "Clinton":
+                    mainCamera.backgroundColor = Color.blue;
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+	
+	// Обновляем один раз за кадр
+	void Update () {
+	
+	}
+}
